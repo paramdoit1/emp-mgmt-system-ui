3c3eaff038361be3bb4ff5f4a114339ee528a9fc
paramdoit <paramdoit@gmail.com>
2019-08-18 18:32:00 +0530

    inital checkin

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Header = styled.h1 `
+    font-family: sans-serif;
+    font-size: 25px;
+    padding-left: 0rem;
+    margin: 1rem;
+    justify-content: center;
+`;
+
+export default Header;
